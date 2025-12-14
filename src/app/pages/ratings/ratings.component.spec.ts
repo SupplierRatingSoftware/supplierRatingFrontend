5d8325ab757f1a18b49f18d5bca6430914035fe4
daes10 <44786122+daes10@users.noreply.github.com>
2025-12-14 23:22:57 +0100

    feat: add ratings component with template, styles, and tests

---
+++ src/app/pages/ratings/ratings.component.spec.ts
@@ -0,0 +1,23 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {RatingsComponent} from './ratings.component';
+
+describe('Ratings', () => {
+  let component: RatingsComponent;
+  let fixture: ComponentFixture<RatingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RatingsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RatingsComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/ratings/ratings.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-ratings',
+  imports: [],
+  templateUrl: './ratings.component.html',
+  styleUrl: './ratings.component.scss',
+})
+export class RatingsComponent {
+
+}
