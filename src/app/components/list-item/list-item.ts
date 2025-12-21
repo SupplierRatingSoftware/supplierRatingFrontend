f967da60ed7e6e2eadede7ce96596d2dc68ab3f5
daes10 <44786122+daes10@users.noreply.github.com>
2025-12-21 21:06:54 +0100

    feat: add, style ListItem component and adding lucide icon

--- src/app/components/list-item/list-item.ts
+++ src/app/components/list-item/list-item.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, UserStar } from 'lucide-angular';
 
 @Component({
   selector: 'app-list-item',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './list-item.html',
   styleUrl: './list-item.scss',
 })
-export class ListItem {}
+export class ListItem {
+  /**
+   * Lucide Icon
+   * @protected
+   */
+  protected readonly UserStar = UserStar;
+}
