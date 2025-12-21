bb580676efa14dae5a894b85a2824813cfd854de
daes10 <44786122+daes10@users.noreply.github.com>
2025-12-21 12:59:37 +0100

    feat: add ListSearch component to suppliers page template
    # Conflicts:
    #	src/app/pages/suppliers/suppliers.component.ts

--- src/app/pages/suppliers/suppliers.component.ts
+++ src/app/pages/suppliers/suppliers.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ListSearch } from '../../components/list-search/list-search';
 import { AddBtn } from '../../components/add-btn/add-btn';
 
 @Component({
   selector: 'app-suppliers',
-  imports: [AddBtn],
+  imports: [ListSearch, AddBtn],
   templateUrl: './suppliers.component.html',
   styleUrl: './suppliers.component.scss',
 })
