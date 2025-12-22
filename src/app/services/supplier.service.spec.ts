3b5ba17daf16f9aabf1cd005dd1a33015710c8ac
daes10 <44786122+daes10@users.noreply.github.com>
2025-12-22 14:49:41 +0100

    feat: rename Supplier class to "SupplierService" and add mocked data with observable

--- src/app/services/supplier.service.spec.ts
+++ src/app/services/supplier.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Supplier } from './supplier';
+import { SupplierService } from './supplier.service';
 
 describe('Supplier', () => {
-  let service: Supplier;
+  let service: SupplierService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Supplier);
+    service = TestBed.inject(SupplierService);
   });
 
   it('should be created', () => {
