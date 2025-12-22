6f2e47bd2ead8144b51796fc4b92783b4f85f26f
daes10 <44786122+daes10@users.noreply.github.com>
2025-12-22 15:21:54 +0100

    feat: integrate SupplierService into SuppliersComponent with initial data loading and state management

--- src/app/pages/suppliers/suppliers.component.ts
+++ src/app/pages/suppliers/suppliers.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { ListSearch } from '../../components/list-search/list-search';
 import { AddBtn } from '../../components/add-btn/add-btn';
 import { ListItem } from '../../components/list-item/list-item';
 import { User } from 'lucide-angular';
+import { SupplierService } from '../../services/supplier.service';
+import { Supplier } from '../../models/supplier.model';
 
 @Component({
   selector: 'app-suppliers',
@@ -11,18 +13,45 @@ import { User } from 'lucide-angular';
   styleUrl: './suppliers.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SuppliersComponent {
+export class SuppliersComponent implements OnInit {
   /**
    * Lucide Icon
    * @protected
    */
   protected readonly User = User;
 
-  // State: List of suppliers
-  readonly suppliers = signal<string[]>(['Lieferant A', 'Lieferant B']);
+  /**
+   * Injected SupplierService
+   * @private
+   */
+  private supplierService: SupplierService = inject(SupplierService);
+
+  // State: Initial List of suppliers
+  readonly suppliers = signal<Supplier[]>([]);
+
+  ngOnInit(): void {
+    this.loadSuppliers();
+  }
+
+  /**
+   * Loads supplier-data from the service and updates the signal state
+   */
+  loadSuppliers() {
+    this.supplierService.getSuppliers().subscribe({
+      next: data => {
+        this.suppliers.set(data);
+        console.log('Suppliers loaded:', data);
+      },
+      error: err => console.error('Error loading suppliers:', err),
+    });
+  }
 
   addSupplier() {
+    /**
+     * TODO: No Backend at the moment to save supplier-data
+     */
+    console.log('Add/Save Supplier clicked');
     // Logic to add a new item to the signal array
-    this.suppliers.update(current => [...current, `Lieferant ${current.length + 1}`]);
+    // this.suppliers.update();
   }
 }
