63e3d940f7f08eb85e5c76dc2f98895448e3a31c
daes10 <44786122+daes10@users.noreply.github.com>
2025-12-22 22:19:23 +0100

    feat: integrate OrderService into OrdersComponent with state management and error handling

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,8 +1,12 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { ListSearch } from '../../components/list-search/list-search';
 import { AddBtn } from '../../components/add-btn/add-btn';
 import { ListItem } from '../../components/list-item/list-item';
 import { NotepadText } from 'lucide-angular';
+import { OrderService } from '../../services/order.service';
+import { Order } from '../../models/order.model';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-orders',
@@ -11,17 +15,63 @@ import { NotepadText } from 'lucide-angular';
   styleUrl: './orders.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
   /**
    * Lucide Icon
    * @protected
    */
   protected readonly NotepadText = NotepadText;
 
-  // State: List of orders
-  readonly orders = signal<string[]>(['Bestellung 1', 'Bestellung 2']);
+  /**
+   * Injected OrderService
+   * @private
+   */
+  private orderService: OrderService = inject(OrderService);
+  private destroyRef = inject(DestroyRef);
 
-  protected addOrder() {
-    this.orders.update(current => [...current, `Bestellung ${current.length + 1}`]);
+  /**
+   * State: Initial list of orders
+   */
+  readonly orders = signal<Order[]>([]);
+
+  /**
+   * State: Error message for UI to display
+   */
+  readonly errorMessage = signal<string | null>(null);
+
+  /**
+   * Lifecycle hook that is called after the component is initialized.
+   */
+  ngOnInit() {
+    this.loadOrders();
+  }
+
+  /**
+   * Loads order-data from the service and updates the signal state
+   * If an error occurs, an error message should be displayed.
+   * @private
+   */
+  private loadOrders() {
+    this.errorMessage.set(null);
+    // simulated error
+    throwError(() => new Error('Simulated Error'));
+    this.orderService
+      .getOrders()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: data => {
+          this.orders.set(data);
+        },
+        error: () => {
+          this.errorMessage.set('Unable to load orders. Please check your connection and try again.');
+        },
+      });
+  }
+
+  /**
+   * TODO: Add openModal functionality and save order-data here
+   */
+  addOrder() {
+    // this.orders.update();
   }
 }
