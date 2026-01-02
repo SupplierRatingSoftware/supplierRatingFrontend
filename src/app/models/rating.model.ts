80ee315363306b31cf1d7e9163bf9520f09b66f0
daes10 <44786122+daes10@users.noreply.github.com>
2026-01-02 19:21:51 +0100

    feat: implement RatingService for managing ratings and create rating functionality

--- src/app/models/rating.model.ts
+++ src/app/models/rating.model.ts
@@ -3,13 +3,23 @@
  */
 
 /**
- * RatingDTO interface
+ * RatingCreateDTO interface
+ * @description DTO for creating a new rating and connect it to an order
+ */
+export interface RatingCreate extends Rating {
+  orderId: string; // Connection to order
+}
+
+/**
+ * RatingDetailDTO interface
  */
 export interface Rating extends RatingBase {
   id: string;
   code: string;
   orderId: string;
   supplierId: string;
+
+  // Optional fields, not marked as "required"
   supplierName?: string;
 }
 
@@ -20,21 +30,13 @@ export interface RatingBase {
   quality: number; // 1-5
   cost: number;
   deadline: number;
-  deadlineReason?: string;
   availability: number;
 
   // Optional fields, not marked as "required"
   qualityReason?: string;
   costReason?: string;
+  deadlineReason?: string;
   availabilityReason?: string;
   totalScore?: number;
   comment?: string;
-}
-
-/**
- * RatingCreateDTO interface
- * @description DTO for creating a new rating and connect it to an order*
- */
-export interface RatingCreate extends RatingBase {
-  orderId: string; // Connection to order
 }
