c55710b574362e36a1bf196c81626be7d3e1ebc3
daes10 <44786122+daes10@users.noreply.github.com>
2026-01-17 21:36:58 +0100

    feat: implement login form with two-way binding and basic authentication
    - Add two-way data binding with `ngModel` for username and password fields.
    - Integrate `onLogin` method with hardcoded credential check for demonstration purposes.
    - Navigate to dashboard on successful login, display alert on failure.
    - Update `LoginComponent` to include necessary imports and signals for form handling.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ThemeSwitcherComponent } from '../../components/theme-switcher/theme-switcher.component';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AppRoutes } from '../../app.routes.config';
 
 @Component({
   selector: 'app-login',
-  imports: [ThemeSwitcherComponent],
+  imports: [ThemeSwitcherComponent, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  /**
+   * Angular Router Service
+   * @private
+   */
+  private router = inject(Router);
+
+  /**
+   * Signals for username and password input fields
+   */
+  readonly username = signal('');
+  readonly password = signal('');
+
+  /**
+   * Method for handling login
+   * TODO: ATTENTION HARDCODED CREDENTIALS ONLY FOR SCHOOL PRESENTATION PURPOSES IMPLEMENTED!!!
+   */
+  onLogin() {
+    console.log(this.username() + ' | ' + this.password());
+    if (this.username() === 'test' && this.password() === 'test') {
+      console.log('Login erfolgreich');
+      this.router.navigate([AppRoutes.BASE, 'dashboard']);
+    } else {
+      alert('Ungültige Zugangsdaten. Bitte versuchen Sie es erneut.');
+    }
+  }
+}
