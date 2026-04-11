dc32adcecf28590b3b3d21d2bc5cfafeb974ae70
AnuschBachofner <61468421+AnuschBachofner@users.noreply.github.com>
2026-04-11 19:21:00 +0200

    Revert "Frontend login (#62)" (#63)
    This reverts commit 5dacb6234b7c4611cfa276ca2bb275898e77cc2b.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './auth.interceptor';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
-    provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
+    provideHttpClient(withFetch()),
   ],
 };
